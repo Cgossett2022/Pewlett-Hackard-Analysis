af765db299a92982db0996324c392e63b8b11a88
Clayton Gossett <111243284+Cgossett2022@users.noreply.github.com>
2022-10-11 21:59:58 -0500

    Updated File
    Updated File to include Deliverable 2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,4 +1,5 @@
 --Employee Database Challenge
+--Deliverable 1
 SELECT e.emp_no, 
 e.first_name, 
 e.last_name, 
@@ -34,7 +35,25 @@ GROUP BY ut.title
 ORDER BY count DESC;
 
 
-
+--Deliverable 2
+--Employees Eligible for the Mentorship Program
+SELECT DISTINCT ON (e.emp_no)
+e.emp_no,
+e.first_name, 
+e.last_name, 
+e.birth_date,
+de.from_date, 
+de.to_date,
+ti.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
 
 
 
